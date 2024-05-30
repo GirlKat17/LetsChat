da40f9c0f77568c8003d172db0d08dc1ab03fb83
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-05-30 23:31:34 +0200

    changing logo

--- lets-chat/letschatup/src/app/components/loading.jsx
+++ lets-chat/letschatup/src/app/components/loading.jsx
@@ -1,7 +1,9 @@
+
+
 export default function Loading() {
     return (
       <div className="page">
-        <div className="logo">☝️</div>
+        <div className="logo">.</div>
         <div className="text">Loading your info...</div>
       </div>
     );
