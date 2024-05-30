6c968e99802ea8040c1e46ecf4ad24556cb6d927
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-05-30 12:13:37 +0200

    created a chat page

---
+++ lets-chat/letschatup/src/app/components/indvChat.jsx
@@ -0,0 +1,22 @@
+import { auth } from "./firebaseConfig";
+import { signOut } from "firebase/auth";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+export default function Page({ user }) {
+  return (
+    <div style={{ height: "100vh" }}>
+      <button
+        style={{ position: "absolute", top: "0px", left: "0px" }}
+        onClick={() => signOut(auth)}
+      >
+        Sign Out
+      </button>
+      <PrettyChatWindow
+        projectId="0d614fab-813f-42ca-840f-bb4bcc4ce20e"
+        username={user.email || ""}
+        secret={user.uid}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+}
