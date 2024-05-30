65133bf7982da8d0de9a7aafb28156be3e5d11a8
Katlego Patience Ramoroa <125869596+GirlKat17@users.noreply.github.com>
2024-05-30 23:31:06 +0200

    updated

--- lets-chat/letschatup/src/app/Auth.jsx
+++ lets-chat/letschatup/src/app/Auth.jsx
@@ -1,24 +1,95 @@
-"use client"
-import { auth } from "./components/firebaseConfig";
+'use client'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { auth } from './components/firebaseConfig';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
 
-export default function AuthPage() {
-  const onClick = () => {
+const AuthPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(null);
+  const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log({ user });
+      setEmail('');
+      setPassword('');
+      router.push('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      await updateProfile(userCredential.user, { displayName: username });
+      console.log({ user: userCredential.user });
+      setEmail('');
+      setPassword('');
+      setUsername('');
+      router.push('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const onClickGoogleSignIn = () => {
     signInWithRedirect(auth, new GoogleAuthProvider());
   };
 
   return (
-    <div className="page">
-      <div className="logo"> 💬 🤖 </div>
-      <div className="text">Welcome to Lets Chat Up</div>
-      <div className="text" style={{ paddingBottom: "16px" }}>
-        Log in with your account to continue
+    <div className='cover'>
+    <div className="container">
+      <div className> 💬 🤖 </div>
+      <div className="text">Chatbox </div>
+      {/* <div>connect with friends easily & quickly</div> */}
+      <div className="container h1" style={{ paddingBottom: "16px" }}>
+        {isLogin ? "Log in to Chatbox Welcome back! Sign in using your social account or email to continue" : "Sign up Get chatting with friends and family today by signing up for our chat app!!"}
       </div>
-      <button className="button" onClick={onClick}>
-        Log In
-      </button> <button className="button" onClick={onClick}>
-        Sign Up
+      <form onSubmit={isLogin ? handleLogin : handleSignUp}>
+        {!isLogin && (
+          <input className='login-input '
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            required
+          />
+        )}
+        <input className='login-input '
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input className='login-input '
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        {error && <p>{error}</p>}
+        <button className='login-button ' type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
+      </form>
+      <button className="button" onClick={onClickGoogleSignIn}>
+        {isLogin ? 'Log In' : 'Sign Up'} with Google
+      </button>
+      <button className="button" onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? 'Switch to Sign Up' : 'Switch to Login'}
       </button>
     </div>
+    </div>
   );
-}
+};
+
+export default AuthPage;
